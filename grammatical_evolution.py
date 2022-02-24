07ca2e25124cb1f19112f2d5fd36015096c3ca13
lennart-rosseburg <71024182+TorroRosso46@users.noreply.github.com>
2022-02-24 20:40:43 +0100

    Update grammatical_evolution.py
    structure idea

--- grammatical_evolution.py
+++ grammatical_evolution.py
@@ -3,3 +3,20 @@ Implementation of the grammatical evolution
 
 """
 
+def grammatical_evolution:
+  def __init__(self):
+
+# mutation
+def mutate():
+  return
+
+# crossover
+def mate():
+  return
+
+# selection
+def select():
+  return
+
+# translate geno- to phenotype
+class Translator:
