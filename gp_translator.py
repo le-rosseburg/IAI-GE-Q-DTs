cbf0e734f4e725513b62b6f5397222515894d219
lennart-rosseburg <lennart.rosseburg@gmail.com>
2022-02-24 21:44:27 +0100

    new gp_translator file

---
+++ gp_translator.py
@@ -0,0 +1,13 @@
+"""
+Implementation of the grammatical evolution translator
+
+"""
+
+# translates geno- to phenotype
+class Translator:
+    def __init__(self):
+        """
+        Initializes a new instance of the Grammatical Evolution
+        :param grammar: A dictionary containing the rules of the grammar and their production
+        """
+        self.operators = grammar
