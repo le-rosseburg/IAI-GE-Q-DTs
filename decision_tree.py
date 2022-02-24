7b92b8a43c4bee0eb0846e9c2db68fb6fde9d909
lennart-rosseburg <71024182+TorroRosso46@users.noreply.github.com>
2022-02-24 20:24:15 +0100

    Update decision_tree.py
    structure idea

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,28 @@
+#!/usr/bin/python3
 """
 Implementation of the decision tree incl. q-learning
 
 """
+import numpy as np
+
+
+class DecisionTree:
+  def __init__(self):
+    
+    
+class Leaf:
+  def __init__(self):
+    
+  def get_action(self):
+    return e_greedy(self)
+    
+  def update(self, reward, q_next):
+    q_learning(self, reward, q_next)
+    
+    
+def q_learning(leaf, reward, q_next):
+  return
+
+
+def e_greedy(leaf):
+  return
