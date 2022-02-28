e435266867766f9e9ee0c7cd8934320c1e374567
lennart-rosseburg <lennart.rosseburg@gmail.com>
2022-02-28 15:05:25 +0100

    uploaded better testrun

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 In this file one can evaluate programs on OpenAI Gym tasks.
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-from .decision_tree import *
-from .grammatical_evolution import *
+from decision_tree import *
+from grammatical_evolution import *
 
 if __name__ == '__main__':
+    print("Hello")
